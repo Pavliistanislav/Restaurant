0483b2fba31f53a1c1899d683c29f5618403693a
PavliiStanislav <Pavliistanislav@users.noreply.github.com>
2020-04-18 19:45:32 +0300

    Responsive && add animation

--- script/main.js
+++ script/main.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
     $('.toggle').toggleClass('open');
     $('.nav-list').toggleClass('open');
   });
+
+  $('.nav-link').on('click', function () {
+    $('.toggle').removeClass('open');
+    $('.nav-list').removeClass('open');
+  });
+
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top - 100,
+      },
+      2000,
+    );
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1000,
+    once: true
+  });
 });
